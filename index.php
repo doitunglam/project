175217fc6b71cca15dc2bb5bd0100a04f8c8b082
Doi Tung Lam <doitunglam1@gmail.com>
2023-02-09 00:15:47 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome</title>
+</head>
+<link rel="stylesheet" href="./styles/style.css">
+<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+<link href='https://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet'>
+<script src="https://kit.fontawesome.com/1a0aa40797.js" crossorigin="anonymous"></script>
+
+<body>
+<div class="main-div">
+    <div class="header-div">
+        <div class="logo-and-link">
+            <a class="logo" href="https://www.w3schools.com">LCH</a>
+            <a class="link" href="https://www.w3schools.com">Publisher</a>
+            <a class="link" href="https://www.w3schools.com">Advertiser</a>
+            <a class="link" href="https://www.w3schools.com">About Us</a>
+
+        </div>
+        <div class="login-logout">
+            <button class="header-login-button" >Log In</button>
+            <button class="header-register-button">Register</button>
+        </div>
+    </div>
+    <div class="bg-image"></div>
+    <div class="sign-in-div">
+            <p class="header">Đăng nhập vào</p>
+            <p class="sitename">localhost.com</p>
+            <p class="desc">Chào mừng đến với trang web, mời bạn nhập thông tin để sử dụng hệ thống</p>
+            <input type="text">
+            <input type="password">
+            <a href="https://www.w3schools.com" class="forgot-password">Quên mật khẩu</a>
+            <button class="login-button">Đăng nhập</button>
+            <p class="connect">Hoặc</p>
+            <div class="third-party-login">
+                <button class="third-party-login-button"><i class="fa-brands fa-facebook"></i> Facebook</button>
+                <button class="third-party-login-button"><i class="fa-brands fa-google"></i> Google</button>
+            </div>
+        <label for="Ghi nhớ đăng nhập" class="remember-me"> <input type="checkbox"> Ghi nhớ đăng nhập </label>
+        </div>
+</div>
+</body>
+</html>
